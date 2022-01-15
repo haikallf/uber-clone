a4871131847f4b36be2b4ec45549a25a89e8be48
haikallf <haikallzrd@gmail.com>
2022-01-15 16:57:30 +0700

    created store and slice

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uberrrr</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Uberr</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
